884531628680f8678baec34cbacebaffeae590b4
Saumya Sakshi <saumyasakshi41@gmail.com>
2025-07-05 08:51:45 +0530

    created the business logic for employee crud

---
+++ src/main/java/com/example/employee_crud/service/EmployeeService.java
@@ -0,0 +1,45 @@
+package com.example.employee_crud.service;
+
+import com.example.employee_crud.Entity.Employee;
+import com.example.employee_crud.repository.EmployeeRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class EmployeeService {
+
+
+    private final EmployeeRepository repository;
+
+    public EmployeeService(EmployeeRepository repo){
+        this.repository=repo;
+    }
+
+    public List<Employee> getEmployees(){
+        return repository.findAll();
+    }
+
+    public Employee getEmployeeById(Long Id)
+    {
+        return repository.findById(Id).orElseThrow(()->new RuntimeException("Employee not found"));
+    }
+    public Employee createEmployee(Employee emp)
+    {
+        return repository.save(emp);
+    }
+    public Employee updateEmployee(Long Id,Employee newEmp)
+    {
+        Employee emp = getEmployeeById(Id);
+        emp.setDepartment(newEmp.getDepartment());
+        emp.setName(newEmp.getName());
+        emp.setSalary(newEmp.getSalary());
+
+        return repository.save(emp);
+    }
+
+    public void deleteEmployee(Long id){
+        repository.deleteById(id);
+    }
+}
